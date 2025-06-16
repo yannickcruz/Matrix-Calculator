5c090df006f86c308f081f453664df9aa819bf06
yannickcruz <yannickcruz02@gmail.com>
2025-06-16 19:15:14 -0300

    Parte da definição de matrizes

--- script/main.js
+++ script/main.js
@@ -1,5 +1,17 @@
-const order = document.getElementById("matrixOrder")
-const matrixName = document.querySelectorAll("#matrixName option");
+let mat = {
+    A: 0,
+    B: 0,
+    C: 0,
+    D: 0
+}
 
-console.log(order.innerHTML.innerHTML);
+function save(){
+    const order = document.getElementById("matrixOrder").value;
+    const matrixName = document.getElementById("matrixName").value;
+    mat[matrixName] = order;
+
+    console.log(`Matriz ${matrixName} agora possui ordem ${order}`);
+
+    
+}
 
