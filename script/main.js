e84baaadfb1752bcc2a8ade35876367141cf8039
yannickcruz <yannickcruz02@gmail.com>
2025-06-22 11:38:11 -0300

    Finalizado o botão de reset e códigos JS foram comentados

--- script/main.js
+++ script/main.js
@@ -13,6 +13,7 @@ let matArr = {
     Cm: [],
     Dm: []
 }
+// Seleciona o botão de salvar os elementos da matriz
 const saveMatrix = document.getElementById("saveMatrix");
 function removeMatrix(){
     const select = document.getElementById("matrixOrder");
@@ -22,7 +23,23 @@ function removeMatrix(){
     select.disabled = false;
 }
 
+function resetMatrix(){
+    //Seleciona o valor do nome da matriz
+    const matrixNameValue = document.getElementById("matrixName").value
+    // Seleciona o Select responsável por alternar os nomes das matrizes
+    const matrixName = document.getElementById("matrixName");
+    // Pega o indice atual do nome da matriz
+    let selected = matrixName.options[matrixName.selectedIndex];
 
+    // Seleciona o nome do atributo que define a matriz escolhida para ser o objeto alvo a ser embutido o array de elementos
+    let matrixArrayId = selected.getAttribute("matrixArray");
+    // Esta ação zera os valores salvos da matriz selecionada
+    matArr[matrixArrayId] = [];
+    // Zera a ordem da matriz
+    mat[matrixNameValue] = 0;
+    // Completa a ação removendo os inputs da matriz
+    removeMatrix();
+}
 function createScalar(){
     const div = document.getElementById("scalar");
     const input = document.createElement("input");
@@ -75,12 +92,14 @@ function create3x3(){
     saveMatrix.style.display = 'block';
 }
 function create4x4(){
+    // Variavel div pega a div especifica para a matriz selecionada
     const div = document.getElementById("matrixAuto4x4");
     const order = parseInt(document.getElementById("matrixOrder").value);
     const label = document.createElement("label");
     label.setAttribute("for", "fill");
     div.appendChild(label);
     //console.log(`Order vale: ${order + 1}`);
+    // Laços de repetição para criar os inputs na organização correta da matriz
     for(let line = 1; line < order + 1; line++){
         for(let col = 1; col < order + 1; col++){
             const input = document.createElement("input");
@@ -91,12 +110,17 @@ function create4x4(){
             console.log(`Criado input referente a posição ${line} ${col}`);
         }
     }
+    // Torna a div selecionada visível
     div.style.display = 'block';
+    // Faz com que o botão de salvar a matriz fique visível
     saveMatrix.style.display = 'block';
 }
 function generateMatrix(){
+    // Remover matriz caso exista uma para evitar conflitos
     removeMatrix();
+    // Variavel que seleciona o Select responsável por variar a ordem da matriz
     const select = document.getElementById("matrixOrder");
+    // Pega o valor da ordem selecionada
     const order = document.getElementById("matrixOrder").value;
     if(order == 1){
         createScalar();
@@ -115,9 +139,13 @@ function generateMatrix(){
 }
 
 function save(){
-    
+    // 'order' pega o valor da ordem da matriz no select de id "matrixOrder"
     const order = document.getElementById("matrixOrder").value;
+
+    // Esta variável pega o valor atual do nome da matriz selecionada
     const matrixNameValue = document.getElementById("matrixName").value;
+
+    // O valor que corresponde ao nome da matriz é identificado dentro do objeto mat, e recebe a ordem da matriz
     mat[matrixNameValue] = order;
 
     console.log(`Matriz ${matrixNameValue} agora possui ordem ${order}`); 
@@ -159,7 +187,7 @@ function next(container){
     }
 });
 }
-//const container = document.querySelector('.matrixAutoCommon');
+
 next(document.getElementById("matrixAuto2x2"));
 next(document.getElementById("matrixAuto3x3"));
 next(document.getElementById("matrixAuto4x4"));
